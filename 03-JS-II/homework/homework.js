03b473e48f4936f633e70a7297e5bfe0cb7177db
Sinonimo <fedesinonimo@gmail.com>
2022-06-27 19:45:08 -0300

    version2.1

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -162,13 +162,15 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
- if(numero <= 1){
+  if(numero < 2){
     return false;  
- }else if(numero % numero === 0 && numero % 1 === 0){
-    return true;
- }else{
-    return false;
  }
+  for(var i=2; numero < 2; i++){
+    if (numero % i === 0){
+      return false;
+    }
+  }
+  return true;
 }
 
 function esVerdadero(valor){
